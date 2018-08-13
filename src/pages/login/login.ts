557daaa4f0ea416e50b1e8f9943d882aeee43e0e
Sean Pragmatic <s@pragmatic-web.co.uk>
2018-08-13 22:02:26 +0100

    removed unnecessary contact pages and added (partically) hard-coded links to password recovery and register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { environment } from '../../environment';
 
 /**
  * Generated class for the LoginPage page.
@@ -11,30 +12,32 @@ import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+	selector: 'page-login',
+	templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  username;
-  password;
+	username;
+	password;
+	register = environment.site_url + '/wp-login.php?action=register';
+	lostPassword = environment.site_url + '/wp-login.php?action=lostpassword';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private authProvider: AuthProvider ) {
-    if( localStorage.getItem( 'wpIonicToken' ) ) {
-      this.navCtrl.setRoot( 'TabsPage' );
-    }
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams, private authProvider: AuthProvider ) {
+		if( localStorage.getItem( 'wpIonicToken' ) ) {
+			this.navCtrl.setRoot( 'TabsPage' );
+		}
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad LoginPage');
+	}
 
-  onLogin() {
-    console.log( this.username, this.password );
-    this.authProvider.postLogin( this.username, this.password ).subscribe( data => {
-      console.log( data );
-      localStorage.setItem( 'wpIonicToken', JSON.stringify( data ) );
-      this.navCtrl.push( 'TabsPage' );
-    });
-  }
+	onLogin() {
+		console.log( this.username, this.password );
+		this.authProvider.postLogin( this.username, this.password ).subscribe( data => {
+			console.log( data );
+			localStorage.setItem( 'wpIonicToken', JSON.stringify( data ) );
+			this.navCtrl.push( 'TabsPage' );
+		});
+	}
 }
