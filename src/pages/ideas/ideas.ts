8608e6c1c031f6d7b011eb998407bc249e2f2f2e
Sean Pragmatic <s@pragmatic-web.co.uk>
2018-08-13 19:22:07 +0100

    added search feature to ideas list, added forgotten password & register link

--- src/pages/ideas/ideas.ts
+++ src/pages/ideas/ideas.ts
@@ -1,7 +1,3 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { IdeasProvider } from '../../providers/ideas/ideas';
-
 /**
  * Generated class for the IdeasPage page.
  *
@@ -9,33 +5,54 @@ import { IdeasProvider } from '../../providers/ideas/ideas';
  * Ionic pages and navigation.
  */
 
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IdeasProvider } from '../../providers/ideas/ideas';
+
 @IonicPage()
 @Component({
-  selector: 'page-ideas',
-  templateUrl: 'ideas.html',
+	selector: 'page-ideas',
+	templateUrl: 'ideas.html',
 })
 export class IdeasPage {
+	ideas;
 
-  ideas;
+	constructor(public navCtrl: NavController, public navParams: NavParams, private ideasProvider: IdeasProvider) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private ideasProvider: IdeasProvider ) {
-    this.ideasProvider.getIdeas().subscribe( data => {
-      console.log( data );
-      this.ideas = data;
-    });
+		this.ideasProvider.getIdeas().subscribe(data => {
+			this.ideas = data;
+			this.initializeItems();
+		});
+	}
 
-  }
+	initializeItems() {
+		this.items = this.ideas;
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad IdeasPage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad IdeasPage');
+	}
 
-  onShowIdeaDetails( idea ) {
-    this.navCtrl.push( 'IdeasDetailPage', { idea: idea } );
-  }
+	onShowIdeaDetails(idea) {
+		this.navCtrl.push('IdeasDetailPage', { idea: idea });
+	}
 
-  onGoToCreateIdea() {
-    this.navCtrl.push( 'CreateIdeaPage' );
-  }
+	onGoToCreateIdea() {
+		this.navCtrl.push('CreateIdeaPage');
+	}
 
+	getItems(ev) {
+		// Reset items back to all of the items
+		this.initializeItems();
+
+		// set val to the value of the ev target
+		var val = ev.target.value;
+
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+			this.items = this.items.filter((item) => {
+				return (item.title.rendered.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+	}
 }
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -3,11 +3,11 @@ import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
 @NgModule({
-  declarations: [
-    LoginPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(LoginPage),
-  ],
+	declarations: [
+		LoginPage,
+	],
+	imports: [
+		IonicPageModule.forChild(LoginPage),
+	],
 })
 export class LoginPageModule {}
